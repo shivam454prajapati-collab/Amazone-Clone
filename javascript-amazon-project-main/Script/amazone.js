e1987c5be4907106da633f81b9a6ddc719098054
Shivam Prajapati <shivam454prajapati@gmail.com>
2025-11-14 00:56:40 +0530

    This Is Script

---
+++ javascript-amazon-project-main/Script/amazone.js
@@ -0,0 +1,32 @@
+const product =[{
+    image:"images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    ratings :{
+        stars: 4.5,
+        count:87
+    },
+    pricecents:1090
+},
+{
+
+    image:"images/products/intermediate-composite-basketball.jpg",
+    name:'Intermediate Size Basketball',
+    ratings:{
+        stars:4,
+        count:127
+    },
+    pricecents:2095
+},
+{
+
+    image:"images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name:'Adults Plain Cotton T-Shirt - 2 Pack',
+    ratings:{
+        stars:4.5,
+        count:50
+    },
+    pricecents:799
+}
+
+
+]
