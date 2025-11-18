b6f673cf3f23b5b8eeef159b2a8b7b802b4a0b47
Shivam Prajapati <shivam454prajapati@gmail.com>
2025-11-18 19:22:55 +0530

    added some feature related to cart

--- javascript-amazon-project-main/Script/amazone.js
+++ javascript-amazon-project-main/Script/amazone.js
@@ -46,7 +46,8 @@ products.forEach((product)=> {
                     Added
                 </div>
 
-                <button class="add-to-cart-button button-primary">
+                <button class="add-to-cart-button button-primary js-add-to-cart"
+                data-product-id="${product.id}">
                     Add to Cart
                 </button>
                 </div>  
@@ -57,6 +58,50 @@ products.forEach((product)=> {
 
 );
 
-console.log(productsHTML);
+
 
 document.querySelector('.js-products-grid').innerHTML=productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+    button.addEventListener('click',()=>{
+      const productId=  button.dataset.productId;            //data set properties will give the all the data attribute to attach that button , & this is converted from kebab case to camel case
+        
+      let matchingItem;
+
+      cart.forEach((item)=>{
+            if (productId===item.productId){
+                matchingItem=item;
+
+            }
+        })
+
+        if(matchingItem){
+            matchingItem.quantity+=1;
+        }else{
+     
+      cart.push({
+        productId:productId,
+        quantity:1
+      })
+    }
+      console.log(cart);
+
+    let cartQuantity=0;
+
+    cart.forEach((item)=>{
+        cartQuantity += item.quantity;
+    })
+
+    document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+    console.log(cartQuantity);
+    console.log(cart);
+        });
+
+
+
+
+
+
+
+
+});
