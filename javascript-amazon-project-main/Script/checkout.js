a9f80ffa51337984fe5eddedf24f0ecb97f26757
Shivam Prajapati <shivam454prajapati@gmail.com>
2025-11-29 01:58:01 +0530

    delivery date add

--- javascript-amazon-project-main/Script/checkout.js
+++ javascript-amazon-project-main/Script/checkout.js
@@ -3,6 +3,7 @@ import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';  //named import we have to use curly braces
 import { hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';  // this is esm module import from direct internet link we dont need write this in script tag in html file we will use it directly here
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';  // this is esm module import from direct internet link & this is also deafault export we can use it when we only want to export 1 thing.
+import { deliveryOptions } from '../data/deliveryOption.js';
 
 
 hello();
@@ -57,50 +58,45 @@ cart.forEach((cartItem) => {
                 <div class="delivery-options-title">
                   Choose a delivery option:
                 </div>
-                <div class="delivery-option">
-                  <input type="radio" checked
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProducts.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Tuesday, June 21
-                    </div>
-                    <div class="delivery-option-price">
-                      FREE Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProducts.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                      $4.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProducts.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                      $9.99 - Shipping
-                    </div>
-                  </div>
-                </div>
+               ${deliveryOptionHTML(matchingProducts,cartItem)}
               </div>
             </div>
           </div> `;
 
           });
+
+          function deliveryOptionHTML(matchingProducts ,cartItem){
+
+            let html='';
+
+            deliveryOptions.forEach((deliveryOption)=>{
+
+              const today=dayjs();
+              const deliveryDate=today.add(deliveryOption.deliveryDays,'day');
+              const dateString=deliveryDate.format('dddd, MMMM D');
+              const priceString=deliveryDate.priceCents===0 ? 'FREE Shipping' : `$${formatCurrency(deliveryOption.priceCents) } - Shipping`;
+
+              const isChecked= deliveryOption.id===cart.deliveryOptionsId
+            html +=   ` <div class="delivery-option">
+                  <input type="radio"
+                  ${isChecked ? 'checked' : ''}
+                    class="delivery-option-input"
+                    name="delivery-option-${matchingProducts.id}">
+                  <div>
+                    <div class="delivery-option-date">
+                      ${dateString}
+                    </div>
+                    <div class="delivery-option-price">
+                     ${priceString}
+                    </div>
+                  </div>
+                </div> `
+          });
+        
+        
+        return html;
+      
+      }
 
           document.querySelector('.js-order-summary').innerHTML=cartSummaryHTML;
 document.querySelectorAll('.js-remove-link').forEach((link)=>{
